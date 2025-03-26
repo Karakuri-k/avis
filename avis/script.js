03319d627b938aa19394c3bb23d75dc48206499a
Karakuri-k <k.kurisakisagberg@gmail.com>
2025-03-26 23:26:18 +0100

    fixed

--- avis/script.js
+++ avis/script.js
@@ -8,21 +8,31 @@ function klikk() {
 }
 
 const darkModeKnapp = document.querySelector(".darkmodeContainer > i")
+const navBar = document.querySelector("nav")
+const settingsWindow = document.querySelector(".settingsWindow")
+const p = document.querySelector("p")
 
 let light = true
+
 darkModeKnapp.addEventListener("click", function() {
     console.log("trykket")
     if (light) {
         document.body.style.backgroundColor = "var(--darkMode)"
+        navBar.style.backgroundColor = "var(--darkAccent)"
+        settingsWindow.style.backgroundColor = "var(--darkAccent)"
+        p.style.color = "var(--lightMode)"
+
         light = false
     } else {
         document.body.style.backgroundColor = "var(--lightMode)"
+        navBar.style.backgroundColor = "var(--lightAccent)"
+        settingsWindow.style.backgroundColor = "var(--lightAccent)"
+        p.style.color = "var(--darkMode)"
         light = true
     }
 })
 
 const settings = document.getElementById("settingsBtn")
-const settingsWindow = document.querySelector(".settingsWindow")
 
 settings.addEventListener("click", function() {
     if (settingsWindow.style.right == "-300px") {
